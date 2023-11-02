b0ae8cc1fdce60dda5aa8a395ddc92fec75e3230
nurulalyh <nurulaliyah2303@gmail.com>
2023-11-02 10:14:09 +0800

    feat authentication and authorization with JWT

--- controllers/distributor.go
+++ controllers/distributor.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 	"pharm-stock/configs"
 	"pharm-stock/helper"
+	"pharm-stock/helper/authentication"
 	"pharm-stock/models"
 	"pharm-stock/utils/request"
 	"pharm-stock/utils/response"
 	"strconv"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
 
@@ -37,25 +39,44 @@ func NewDistributorControllerInterface(m models.DistributorModelInterface) Distr
 // Create Distributor
 func (dc *DistributorsController) CreateDistributor() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var input = request.DistributorsRequest{}
-		if errBind := c.Bind(&input); errBind != nil {
-			return c.JSON(http.StatusBadRequest, helper.FormatResponse("invalid distributor input", errBind.Error()))
+		userToken := c.Get("user").(*jwt.Token)
+
+		if userToken != nil && userToken.Valid {
+			tokenData, err := authentication.ExtractToken(userToken)
+			if err != nil {
+				return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Invalid token", err.Error()))
+			}
+
+			role, ok := tokenData["role"].(string)
+			if !ok {
+				return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Role information missing in the token", nil))
+			}
+
+			if role != "administrator" {
+				return c.JSON(http.StatusUnauthorized, helper.FormatResponse("You don't have permission", nil))
+			}
+
+			var input = request.DistributorsRequest{}
+			if errBind := c.Bind(&input); errBind != nil {
+				return c.JSON(http.StatusBadRequest, helper.FormatResponse("invalid distributor input", errBind.Error()))
+			}
+	
+			var newDistributor = models.Distributors{}
+			newDistributor.Name = input.Name
+	
+			var res, errQuery = dc.model.Insert(newDistributor)
+			if res == nil {
+				return c.JSON(http.StatusInternalServerError, helper.FormatResponse("Cannot process data, something happend", errQuery.Error()))
+			}
+	
+			var insertResponse = response.InsertDistributorResponse{}
+			insertResponse.Id = res.Id
+			insertResponse.Name = res.Name
+			insertResponse.CreatedAt = res.CreatedAt
+	
+			return c.JSON(http.StatusCreated, helper.FormatResponse("success create distributor", insertResponse))
 		}
-
-		var newDistributor = models.Distributors{}
-		newDistributor.Name = input.Name
-
-		var res, errQuery = dc.model.Insert(newDistributor)
-		if res == nil {
-			return c.JSON(http.StatusInternalServerError, helper.FormatResponse("Cannot process data, something happend", errQuery.Error()))
-		}
-
-		var insertResponse = response.InsertDistributorResponse{}
-		insertResponse.Id = res.Id
-		insertResponse.Name = res.Name
-		insertResponse.CreatedAt = res.CreatedAt
-
-		return c.JSON(http.StatusCreated, helper.FormatResponse("success create distributor", insertResponse))
+		return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Unauthorized access", nil))
 	}
 }
 
@@ -77,40 +98,78 @@ func (dc *DistributorsController) GetAllDistributor() echo.HandlerFunc {
 // Update distributor
 func (dc *DistributorsController) UpdateDistributor() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var paramId = c.Param("id")
-		var input = models.Distributors{}
-		if errBind := c.Bind(&input); errBind != nil {
-			return c.JSON(http.StatusBadRequest, helper.FormatResponse("invalid distributor input", errBind.Error()))
+		userToken := c.Get("user").(*jwt.Token)
+
+		if userToken != nil && userToken.Valid {
+			tokenData, err := authentication.ExtractToken(userToken)
+			if err != nil {
+				return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Invalid token", err.Error()))
+			}
+
+			role, ok := tokenData["role"].(string)
+			if !ok {
+				return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Role information missing in the token", nil))
+			}
+
+			if role != "administrator" {
+				return c.JSON(http.StatusUnauthorized, helper.FormatResponse("You don't have permission", nil))
+			}
+
+			var paramId = c.Param("id")
+			var input = models.Distributors{}
+			if errBind := c.Bind(&input); errBind != nil {
+				return c.JSON(http.StatusBadRequest, helper.FormatResponse("invalid distributor input", errBind.Error()))
+			}
+	
+			input.Id = paramId
+	
+			var res, errQuery = dc.model.Update(input)
+			if res == nil {
+				return c.JSON(http.StatusInternalServerError, helper.FormatResponse("cannot process data, something happend", errQuery.Error()))
+			}
+	
+			updateResponse := response.UpdateDistributorResponse{}
+			updateResponse.Id = res.Id
+			updateResponse.Name = res.Name
+			updateResponse.CreatedAt = res.CreatedAt
+			updateResponse.UpdatedAt = res.UpdatedAt
+	
+			return c.JSON(http.StatusOK, helper.FormatResponse("Success update data", updateResponse))
 		}
+		return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Unauthorized access", nil))
 
-		input.Id = paramId
-
-		var res, errQuery = dc.model.Update(input)
-		if res == nil {
-			return c.JSON(http.StatusInternalServerError, helper.FormatResponse("cannot process data, something happend", errQuery.Error()))
-		}
-
-		updateResponse := response.UpdateDistributorResponse{}
-		updateResponse.Id = res.Id
-		updateResponse.Name = res.Name
-		updateResponse.CreatedAt = res.CreatedAt
-		updateResponse.UpdatedAt = res.UpdatedAt
-
-		return c.JSON(http.StatusOK, helper.FormatResponse("Success update data", updateResponse))
 	}
 }
 
 // Delete Distributor
 func (dc *DistributorsController) DeleteDistributor() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var paramId = c.Param("id")
+		userToken := c.Get("user").(*jwt.Token)
 
-		success, errQuery := dc.model.Delete(paramId)
-		if !success {
-			return c.JSON(http.StatusNotFound, helper.FormatResponse("distributor not found", errQuery.Error()))
+		if userToken != nil && userToken.Valid {
+			tokenData, err := authentication.ExtractToken(userToken)
+			if err != nil {
+				return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Invalid token", err.Error()))
+			}
+
+			role, ok := tokenData["role"].(string)
+			if !ok {
+				return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Role information missing in the token", nil))
+			}
+
+			if role != "administrator" {
+				return c.JSON(http.StatusUnauthorized, helper.FormatResponse("You don't have permission", nil))
+			}
+			var paramId = c.Param("id")
+	
+			success, errQuery := dc.model.Delete(paramId)
+			if !success {
+				return c.JSON(http.StatusNotFound, helper.FormatResponse("distributor not found", errQuery.Error()))
+			}
+	
+			return c.JSON(http.StatusOK, helper.FormatResponse("Success delete distributor", nil))
 		}
-
-		return c.JSON(http.StatusOK, helper.FormatResponse("Success delete distributor", nil))
+		return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Unauthorized access", nil))
 	}
 }
 
--- helper/authentication/jwt.go
+++ helper/authentication/jwt.go
@@ -2,9 +2,7 @@ package authentication
 
 import (
 	"errors"
-	"net/http"
 	"pharm-stock/configs"
-	"pharm-stock/helper"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -12,96 +10,32 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func generateToken(signKey string, id string, username string, role string) (string, error) {
-	var claims = jwt.MapClaims{}
-	claims["id"] = id
+func GenerateToken(id string, username string, role string) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["userId"] = id
 	claims["username"] = username
 	claims["role"] = role
 	claims["iat"] = time.Now().Unix()
-	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
-
-	var sign = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	validToken, err := sign.SignedString([]byte(signKey))
-	if err != nil {
-		return "", errors.New("JWT claims isn't valid, " + err.Error())
-	}
-
-	return validToken, nil
+	claims["exp"] = time.Now().Add(time.Minute * 1).Unix() //Token expires after 1 hour
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	cfg := configs.Config{}
+	return token.SignedString([]byte(cfg.Secret))
 }
 
-func generateRefreshToken(signKey string, accessToken string) (string, error) {
-	var claims = jwt.MapClaims{}
-	claims["user"] = accessToken
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-
-	var sign = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	refreshToken, err := sign.SignedString([]byte(signKey))
-	if err != nil {
-		return "", errors.New("JWT claims isn't valid, " + err.Error())
+func ExtractTokenUserId(e echo.Context) (string, string, string) {
+	user := e.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := claims["userId"].(string)
+		userName := claims["user_name"].(string)
+		role := claims["role"].(string)
+		return userId, userName, role
 	}
-
-	return refreshToken, nil
+	return "", "", ""
 }
 
-func GenerateJWT(signKey string, refreshKey string, userId string, username string, role string) (map[string]any, error) {
-	var res = map[string]any{}
-
-	var accessToken, errGenerateToken = generateToken(signKey, userId, username, role)
-	if accessToken == "" {
-		return nil, errors.New("Cannot generate JWT token, " + errGenerateToken.Error())
-	}
-
-	var refreshToken, errRefToken = generateRefreshToken(refreshKey, accessToken)
-	if refreshToken == "" {
-		return nil, errors.New("Cannot generate JWT refresh token, " + errRefToken.Error())
-	}
-
-	res["access_token"] = accessToken
-	res["refresh_token"] = refreshToken
-
-	return res, nil
-}
-
-func RefreshJWT(accessToken string, refreshToken *jwt.Token, signKey string) (map[string]any, error) {
-	var res = map[string]any{}
-
-	expTime, err := refreshToken.Claims.GetExpirationTime()
-	if err != nil {
-		return nil, errors.New("Error get token expiration, " + err.Error())
-	}
-
-	if refreshToken.Valid && expTime.After(time.Now()) {
-		var newClaim = jwt.MapClaims{}
-
-		newToken, err := jwt.ParseWithClaims(accessToken, newClaim, func(t *jwt.Token) (interface{}, error) {
-			return []byte(signKey), nil
-		})
-		if err != nil {
-			return nil, errors.New("JWT error : " + err.Error())
-		}
-
-		newClaim = newToken.Claims.(jwt.MapClaims)
-		newClaim["iat"] = time.Now().Unix()
-		newClaim["exp"] = time.Now().Add(time.Hour * 1).Unix()
-
-		var newRefreshClaim = refreshToken.Claims.(jwt.MapClaims)
-		newRefreshClaim["exp"] = time.Now().Add(time.Hour * 24).Unix()
-
-		var newRefreshToken = jwt.NewWithClaims(refreshToken.Method, newRefreshClaim)
-		newSignedRefreshToken, _ := newRefreshToken.SignedString(refreshToken.Signature)
-
-		res["access_token"] = newToken.Raw
-		res["refresh_token"] = newSignedRefreshToken
-
-		return res, nil
-	}
-
-	return nil, nil
-}
-
-func ExtractToken(token *jwt.Token) (any, error) {
+func ExtractToken(token *jwt.Token) (map[string]any, error) {
 	if token.Valid {
 		var claims = token.Claims
 
@@ -117,7 +51,6 @@ func ExtractToken(token *jwt.Token) (any, error) {
 
 		return nil, errors.New("JWT token expired")
 	}
-
 	return nil, nil
 }
 
@@ -129,22 +62,131 @@ func Middleware() echo.MiddlewareFunc {
 	})
 }
 
-func JWTMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	cfg := configs.Config{}
-	return func(c echo.Context) error {
-		tokenString := c.Request().Header.Get("Authorization")
-		if tokenString == "" {
-			return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Token is required", nil))
-		}
+// func generateToken(signKey string, id string, username string, role string) (string, error) {
+// 	var claims = jwt.MapClaims{}
+// 	claims["id"] = id
+// 	claims["username"] = username
+// 	claims["role"] = role
+// 	claims["iat"] = time.Now().Unix()
+// 	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
 
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return []byte(cfg.Secret), nil
-		})
+// 	var sign = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-		if err != nil || !token.Valid {
-			return c.JSON(http.StatusUnauthorized, helper.FormatResponse("Invalid token", err))
-		}
+// 	validToken, err := sign.SignedString([]byte(signKey))
+// 	if err != nil {
+// 		return "", errors.New("JWT claims isn't valid, " + err.Error())
+// 	}
 
-		return next(c)
-	}
-}
+// 	return validToken, nil
+// }
+
+// func generateRefreshToken(signKey string, accessToken string) (string, error) {
+// 	var claims = jwt.MapClaims{}
+// 	claims["user"] = accessToken
+// 	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+// 	var sign = jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+// 	refreshToken, err := sign.SignedString([]byte(signKey))
+// 	if err != nil {
+// 		return "", errors.New("JWT claims isn't valid, " + err.Error())
+// 	}
+
+// 	return refreshToken, nil
+// }
+
+// func GenerateJWT(signKey string, refreshKey string, userId string, username string, role string) (map[string]any, error) {
+// 	var res = map[string]any{}
+
+// 	var accessToken, errGenerateToken = generateToken(signKey, userId, username, role)
+// 	if accessToken == "" {
+// 		return nil, errors.New("Cannot generate JWT token, " + errGenerateToken.Error())
+// 	}
+
+// 	var refreshToken, errRefToken = generateRefreshToken(refreshKey, accessToken)
+// 	if refreshToken == "" {
+// 		return nil, errors.New("Cannot generate JWT refresh token, " + errRefToken.Error())
+// 	}
+
+// 	res["access_token"] = accessToken
+// 	res["refresh_token"] = refreshToken
+
+// 	return res, nil
+// }
+
+// func RefreshJWT(accessToken string, refreshToken *jwt.Token, signKey string) (map[string]any, error) {
+// 	var res = map[string]any{}
+
+// 	expTime, err := refreshToken.Claims.GetExpirationTime()
+// 	if err != nil {
+// 		return nil, errors.New("Error get token expiration, " + err.Error())
+// 	}
+
+// 	if refreshToken.Valid && expTime.After(time.Now()) {
+// 		var newClaim = jwt.MapClaims{}
+
+// 		newToken, err := jwt.ParseWithClaims(accessToken, newClaim, func(t *jwt.Token) (interface{}, error) {
+// 			return []byte(signKey), nil
+// 		})
+// 		if err != nil {
+// 			return nil, errors.New("JWT error : " + err.Error())
+// 		}
+
+// 		newClaim = newToken.Claims.(jwt.MapClaims)
+// 		newClaim["iat"] = time.Now().Unix()
+// 		newClaim["exp"] = time.Now().Add(time.Hour * 1).Unix()
+
+// 		var newRefreshClaim = refreshToken.Claims.(jwt.MapClaims)
+// 		newRefreshClaim["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+// 		var newRefreshToken = jwt.NewWithClaims(refreshToken.Method, newRefreshClaim)
+// 		newSignedRefreshToken, _ := newRefreshToken.SignedString(refreshToken.Signature)
+
+// 		res["access_token"] = newToken.Raw
+// 		res["refresh_token"] = newSignedRefreshToken
+
+// 		return res, nil
+// 	}
+
+// 	return nil, nil
+// }
+
+// func ExtractTokenUserId(e echo.Context) (string, string, string) {
+// 	user := e.Get("user").(*jwt.Token)
+// 	if user.Valid {
+// 		claims := user.Claims.(jwt.MapClaims)
+// 		userId := claims["userId"].(string)
+// 		userName := claims["user_name"].(string)
+// 		role := claims["role"].(string)
+// 		return userId, userName, role
+// 	}
+// 	return "", "", ""
+// }
+
+// func ExtractToken(token *jwt.Token) (any, error) {
+// 	if token.Valid {
+// 		var claims = token.Claims
+
+// 		expTime, _ := claims.GetExpirationTime()
+// 		if expTime.After(time.Now()) {
+// 			var MapClaim = claims.(jwt.MapClaims)
+// 			newMap := map[string]any{}
+// 			newMap["id"] = MapClaim["id"]
+// 			newMap["username"] = MapClaim["username"]
+// 			newMap["role"] = MapClaim["role"]
+// 			return newMap, nil
+// 		}
+
+// 		return nil, errors.New("JWT token expired")
+// 	}
+
+// 	return nil, nil
+// }
+
+// func Middleware() echo.MiddlewareFunc {
+// 	cfg := configs.Config{}
+// 	return echojwt.WithConfig(echojwt.Config{
+// 		SigningKey:    []byte(cfg.Secret),
+// 		SigningMethod: "HS256",
+// 	})
+// }
